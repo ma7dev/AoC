4455b74bc44a0dfbf76eaa489c50b4c61c3e8be2
sudomaze <mazen.ota@gmail.com>
2021-12-04 15:46:24 -0800

    completed 5, 2015

--- 2015/5/main.py
+++ 2015/5/main.py
@@ -1,11 +1,9 @@
-# TODO:
 import sys
 sys.path.insert(0, "../../")
 from helpers import utils
 # --------------------------------------------------
 import csv
 import copy
-import hashlib
 from typing import List
 def read(filename: str) -> List[str]: 
     data = []
@@ -60,46 +58,41 @@ def part_1(data,verbose=False):
 
 def part_2(data,verbose=False):
     utils.start_timer()
-    vowels = "aeiou"
-    bad = ["ab", "cd", "pq", "xy"]
     def check(word,verbose):
-        vowel_count = 0
-        prev = word[0]
-        if prev in vowels:
-            vowel_count += 1
         nice = [False,False]
         statement = []
-        for char in word[1:]:
-            if char in vowels:
-                vowel_count += 1
-                if vowel_count == 3:
-                    statement.append("at least 3 vowels")
+        for i in range(len(word)-2):
+            target = f"{word[i]}{word[i+1]}"
+            for j in range(i+2,len(word)-1):
+                if f"{word[j]}{word[j+1]}" == target:
+                    statement.append(f"{target} repeated twice")
                     nice[0] = True
-            if char == prev:
-                statement.append(f"a double letter - {prev}{char}")
+                    break
+            if nice[0]:
+                break
+        for i, char in enumerate(word[:-2]):
+            if word[i] == word[i+2]:
+                statement.append(f"{word[i]}{word[i+1]}{word[i+2]}")
                 nice[1] = True
-            elif f"{prev}{char}" in bad:
-                if verbose:
-                    utils.pprint(1, f"False: {prev}{char}")
-                return False
-            prev = char
+                break
         if all(nice):
             if verbose:
-                utils.pprint(1, f"True: {', '.join(statement)}")
+                utils.pprint(2, f"True: {', '.join(statement)}")
             return True
         else:
             if verbose:
-                utils.pprint(1,f"False: not nice")
+                utils.pprint(2,f"False: not nice {nice}")
             return False
     counter = 0
     for word in data:
         word = list(word)
         if check(word,verbose):
             counter += 1
-    utils.pprint(1, counter)
+    utils.pprint(2, counter)
 
-
-data = read('input.txt')
-# data = ["ugknbfddgicrmopn", "aaa", "jchzalrnumimnmhp", "haegwjzuvuyypxyu","dvszwmarrgswjxmb"]
-part_1(data)
-# part_2(data)
+if __name__ == "__main__":
+    # data = ["ugknbfddgicrmopn", "aaa", "jchzalrnumimnmhp", "haegwjzuvuyypxyu","dvszwmarrgswjxmb"]
+    # data = ["qjhvhtzxzqqjkmpb", "xxyxx","uurcxstgmygtbstg","ieodomkazucvgmuy"]
+    data = read('input.txt')
+    part_1(data)
+    part_2(data)
