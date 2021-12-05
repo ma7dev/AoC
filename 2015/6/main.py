789204f9b15abf101e4d2488c3e4e596dd2bf1dc
sudomaze <mazen.ota@gmail.com>
2021-12-05 12:17:18 -0800

    Completed Day 6, 2015 -- need refactoring

--- 2015/6/main.py
+++ 2015/6/main.py
@@ -13,23 +13,14 @@ def read(filename: str) -> List[str]:
     with open(filename, 'r') as f:
         for _, row in enumerate(f):
             splitter = row.split()
-            # print(splitter)
             if "turn" in splitter:
                 if splitter[1] == "on":
-                    # print(f"turn, on {[1,[int(x) for x in splitter[2].split(',')],[int(x) for x in splitter[4].split(',')]]}")
                     data.append([1,[int(x) for x in splitter[2].split(',')],[int(x) for x in splitter[4].split(',')]])
                 else:
-                    # print(f"turn, off {[0,[int(x) for x in splitter[2].split(',')],[int(x) for x in splitter[4].split(',')]]}")
                     data.append([0,[int(x) for x in splitter[2].split(',')],[int(x) for x in splitter[4].split(',')]])
             else:
-                # print(f"toggle {[-1,[int(x) for x in splitter[1].split(',')],[int(x) for x in splitter[3].split(',')]]}")
                 data.append([-1,[int(x) for x in splitter[1].split(',')],[int(x) for x in splitter[3].split(',')]])
     return data
-def validate(data):
-    for cmd in data:
-        # pdb.set_trace()
-        if cmd[1][0] > cmd[2][0] or cmd[1][1] > cmd[2][1]:
-            print(cmd)
 def part_1(data,verbose=False):
     utils.start_timer()
     arr = np.zeros([1000,1000])
@@ -45,14 +36,17 @@ def part_1(data,verbose=False):
                 action[1][1]:action[2][1]+1
             ] = 0
         else:
-            ones = np.where(arr[
+            arr_ones = np.ones([1000,1000]) * -1
+            arr_ones[
                 action[1][0]:action[2][0]+1,
                 action[1][1]:action[2][1]+1
-            ] == 1)
-            zeros = np.where(arr[
+            ] = 1
+            ones = np.equal(arr,arr_ones)
+            arr_ones[
                 action[1][0]:action[2][0]+1,
                 action[1][1]:action[2][1]+1
-            ] == 0)
+            ] = 0
+            zeros = np.equal(arr,arr_ones)
             arr[zeros] = 1
             arr[ones] = 0
         if verbose:
@@ -62,84 +56,41 @@ def part_1(data,verbose=False):
 
 def part_2(data,verbose=False):
     utils.start_timer()
-    utils.pprint(2, "")
-
-# data = read('ex.txt')
-data = read('input.txt')
-validate(data)
-# print(data)
-# part_1(data,verbose=True)
-# part_2(data)
-
-# TODO:
-import sys
-sys.path.insert(0, "../../")
-from helpers import utils
-# --------------------------------------------------
-import numpy as np
-from typing import List
-import pdb
-def read(filename: str) -> List[str]: 
-    data = []
-    with open(filename, 'r') as f:
-        for _, row in enumerate(f):
-            splitter = row.split()
-            # print(splitter)
-            if "turn" in splitter:
-                if splitter[1] == "on":
-                    # print(f"turn, on {[1,[int(x) for x in splitter[2].split(',')],[int(x) for x in splitter[4].split(',')]]}")
-                    data.append([1,[int(x) for x in splitter[2].split(',')],[int(x) for x in splitter[4].split(',')]])
-                else:
-                    # print(f"turn, off {[0,[int(x) for x in splitter[2].split(',')],[int(x) for x in splitter[4].split(',')]]}")
-                    data.append([0,[int(x) for x in splitter[2].split(',')],[int(x) for x in splitter[4].split(',')]])
-            else:
-                # print(f"toggle {[-1,[int(x) for x in splitter[1].split(',')],[int(x) for x in splitter[3].split(',')]]}")
-                data.append([-1,[int(x) for x in splitter[1].split(',')],[int(x) for x in splitter[3].split(',')]])
-    return data
-def validate(data):
-    for cmd in data:
-        if cmd[1][0] > cmd[2][0]:
-            print(cmd)
-        if cmd[1][1] > cmd[2][1]:
-            print(cmd)
-def part_1(data,verbose=False):
-    utils.start_timer()
     arr = np.zeros([1000,1000])
     for action in data:
-        pdb.set_trace()
         if action[0] == 1:
             arr[
                 action[1][0]:action[2][0]+1,
                 action[1][1]:action[2][1]+1
-            ] = 1
+            ] += 1
         elif action[0] == 0:
             arr[
                 action[1][0]:action[2][0]+1,
                 action[1][1]:action[2][1]+1
-            ] = 0
+            ] -= 1
+            arr[np.where(arr < 0)] = 0
         else:
-            ones = np.where(arr[
+            arr[
                 action[1][0]:action[2][0]+1,
                 action[1][1]:action[2][1]+1
-            ] == 1)
-            zeros = np.where(arr[
-                action[1][0]:action[2][0]+1,
-                action[1][1]:action[2][1]+1
-            ] == 0)
-            arr[zeros] = 1
-            arr[ones] = 0
+            ] += 2
         if verbose:
-            utils.pprint(1, arr[np.where(arr == 1)].size)
-    utils.pprint(1, arr[np.where(arr == 1)].size)
-        
+            utils.pprint(2, np.sum(arr[np.where(arr>0)]))
+    utils.pprint(2, np.sum(arr[np.where(arr>0)]))
 
-def part_2(data,verbose=False):
-    utils.start_timer()
-    utils.pprint(2, "")
-
-data = read('ex.txt')
-# data = read('input.txt')
-# validate(data)
-# print(data)
-part_1(data,verbose=True)
-# part_2(data)
+if __name__ == "__main__":
+    print("Advent of Code 2015 - Day 6")
+    print("-----------------------------")
+    print("Example 1:")
+    data = read('ex1.txt')
+    part_1(data)
+    part_2(data)
+    print("Example 1:")
+    data = read('ex2.txt')
+    part_1(data)
+    part_2(data)
+    print("-----------------------------")
+    print("Test:")
+    data = read('input.txt')
+    part_1(data)
+    part_2(data)
