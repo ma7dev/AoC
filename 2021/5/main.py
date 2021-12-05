229743fe052c9c3b4e01dfcf3c4e6f19bd2ce9e0
sudomaze <mazen.ota@gmail.com>
2021-12-05 11:20:18 -0800

    Refactored Day 5, 2021 - inspired by Mohad12211's refactoring

--- 2021/5/main.py
+++ 2021/5/main.py
@@ -32,17 +32,8 @@ def part_1(data,max_val, max_val_y,verbose=False):
     utils.start_timer()
     arr = np.zeros((max_val+1, max_val_y+1))
     for r in data:
-        if r[0] == r[2]:
-            # print(r)
-            if r[1] <= r[3]:
-                arr[r[0], r[1]:r[3]+1] += 1
-            else:
-                arr[r[0], r[3]:r[1]+1] += 1
-        elif r[1] == r[3]:
-            if r[0] <= r[2]:
-                arr[r[0]:r[2]+1, r[1]] += 1
-            else:
-                arr[r[2]:r[0]+1, r[1]] += 1
+        if r[0] == r[2] or r[1] == r[3]:
+            arr[min(r[0],r[2]):max(r[0],r[2])+1, min(r[1],r[3]):max(r[1],r[3])+1] += 1
     if verbose:
         print(arr.T)
     utils.pprint(1, len(arr[np.where(arr >= 2)]))
@@ -52,39 +43,17 @@ def part_2(data,max_val, max_val_y,verbose=False):
     utils.start_timer()
     arr = np.zeros((max_val+1, max_val_y+1))
     for r in data:
-        if r[0] == r[2]:
-            if r[1] < r[3]:
-                arr[r[0], r[1]:r[3]+1] += 1
-            else:
-                arr[r[0], r[3]:r[1]+1] += 1
-        elif r[1] == r[3]:
-            if r[0] < r[2]:
-                arr[r[0]:r[2]+1, r[1]] += 1
-            else:
-                arr[r[2]:r[0]+1, r[1]] += 1
+        if r[0] == r[2] or r[1] == r[3]:
+            arr[min(r[0],r[2]):max(r[0],r[2])+1, min(r[1],r[3]):max(r[1],r[3])+1] += 1
         else:
-            if r[0] < r[2]:
-                if r[1] < r[3]:
-                    i = 0
-                    while r[0]+i < r[2]+1:
-                        arr[r[0]+i, r[1]+i] += 1
-                        i += 1
-                else:
-                    i = 0
-                    while r[0]+i < r[2]+1:
-                        arr[r[0]+i, r[1]-i] += 1
-                        i += 1
-            else:
-                if r[1] <= r[3]:
-                    i = 0
-                    while r[2]+i < r[0]+1:
-                        arr[r[0]-i, r[1]+i] += 1
-                        i += 1
-                else:
-                    i = 0
-                    while r[2]+i < r[0]+1:
-                        arr[r[0]-i, r[1]-i] += 1
-                        i += 1
+            x = np.sign(r[2]-r[0])
+            y = np.sign(r[3]-r[1])
+            if x == 0:
+                x = 1
+            if y == 0:
+                y = 1
+            for i in range(np.abs(r[2]-r[0])):
+                arr[r[0]+(i*x), r[1]+(i*y)] += 1
     if verbose:
         print(arr.T)
     utils.pprint(2, len(arr[np.where(arr >= 2)]))
